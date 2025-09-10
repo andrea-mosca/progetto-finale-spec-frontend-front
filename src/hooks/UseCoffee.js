1305ebf8ecde5f172f1bb759580a61c0b9a80587
andrea-mosca <andrea.moscateli@gmail.com>
2025-09-10 12:10:10 +0200

    details page

--- src/hooks/UseCoffee.js
+++ src/hooks/UseCoffee.js
@@ -1,20 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 const apiUrl = import.meta.env.VITE_API_URL;
 export default function useCoffee() {
   const [coffee, setCoffee] = useState([]);
+  const [singleCoffee, setSingleCoffee] = useState([]);
+  // chiamata coffee list
   useEffect(() => {
     const fetchCoffee = async () => {
       try {
         const response = await fetch(`${apiUrl}/coffees`);
         const data = await response.json();
         setCoffee(data);
-        console.log(coffee);
       } catch (err) {
         console.error(err);
       }
     };
     fetchCoffee();
   }, []);
+  // funzione per singolo elemento
+  const fetchSingleCoffee = useCallback(async (id) => {
+    try {
+      const response = await fetch(`${apiUrl}/coffees/${id}`);
+      const data = await response.json();
+      setSingleCoffee(data.coffee);
+      return singleCoffee;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  });
 
-  return { coffee, setCoffee };
+  return { coffee, setCoffee, fetchSingleCoffee, singleCoffee };
 }
--- src/pages/CoffeeDetails.jsx
+++ src/pages/CoffeeDetails.jsx
@@ -1,17 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useCoffeeContext } from "../context/CoffeeContext";
+import { useEffect } from "react";
 
 export default function CoffeDetails() {
-  const { coffee } = useCoffeeContext();
+  const { singleCoffee, fetchSingleCoffee } = useCoffeeContext();
   const { id } = useParams();
-  const singleCoffe = coffee.find((c) => String(c.id) === id);
+  console.log(singleCoffee);
 
-  if (!singleCoffe) {
+  useEffect(() => {
+    fetchSingleCoffee(id);
+  }, [id]);
+  if (!singleCoffee) {
     return <h1>Il tuo caffè sta caricando o non è stato trovato... </h1>;
   }
   return (
     <div className="container">
-      <h1>{singleCoffe.title}</h1>
+      <div className="details-card mt-5">
+        <h1>{singleCoffee.title} </h1>
+        <p>
+          <strong>Categoria:</strong> {singleCoffee.category}
+        </p>
+        <p>
+          <strong>Origine:</strong> {singleCoffee.origine}
+        </p>
+        <p>
+          <strong>Intensità:</strong> {singleCoffee.intensita} /10
+        </p>
+        <p>
+          <strong>Caffeina:</strong> {singleCoffee.caffeina} mg
+        </p>
+        <p>
+          <strong>Prezzo:</strong> {singleCoffee.prezzo} € /kg
+        </p>
+        <p>
+          <strong>Tostatura:</strong> {singleCoffee.tostatura}
+        </p>
+        <p>
+          <strong>Descrizione:</strong> {singleCoffee.descrizione}
+        </p>
+      </div>
     </div>
   );
 }
