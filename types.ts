d5f70fd948bcf2abf3c447bffc33063b1ec403ea
andrea-mosca <andrea.moscateli@gmail.com>
2025-09-24 11:07:13 +0200

    final push(database & types.ts)

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type Coffee={
+    title: string,          
+    category: string,      
+    origine: string,       
+    intensita: number,      
+    caffeina: number,     
+    prezzo: number, 
+    tostatura?: "Chiara" | "Media" | "Scura",
+    image: string,
+    descrizione: string
+}
